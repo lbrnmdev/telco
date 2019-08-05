a9b71b36f063e382e989b637f4b5304448724a8e
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-08-06 00:16:27 +0200

    add basic cowboy start to telco app

--- apps/telco/src/telco_app.erl
+++ apps/telco/src/telco_app.erl
@@ -10,7 +10,13 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    telco_sup:start_link().
+  Dispatch = cowboy_router:compile([
+                                    {'_', [
+                                           {"/", telco_h, []}
+                                          ]}
+                                   ]),
+  {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
+  telco_sup:start_link().
 
 stop(_State) ->
     ok.
